6367065cb0a441a6e6701c36a31dcd977e519e65
Ikram Khokhar <130503059+ikram-fullstackdev@users.noreply.github.com>
2025-07-12 01:06:44 +0500

    feat: added createTodo mutation and isComplete property

--- api/index.js
+++ api/index.js
@@ -1,21 +1,38 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+
 const typeDefs = `#graphql
   type ToDo {
     id: ID
     text: String
+    isComplete: Boolean
   }
 
   type Query {
     todos: [ToDo]
   }
+
+  type Mutation {
+    createTodo(text: String!, isComplete: Boolean!): ToDo
+  }
+  
 `;
+
+const todos = [{ id: 1, text: 'hello world', isComplete: false }];
 
 const resolvers = {
   Query: {
-    todos: () => [{ id: 1, text: 'hello world' }],
+    todos: () => todos,
   },
+  Mutation: {
+    createTodo: (_, { text, isComplete }) => {
+      const newTodo = { id: todos.length + 1, text, isComplete };
+      todos.push(newTodo);
+      console.log('New todo created:', newTodo, todos);
+      return newTodo;
+    },
+  }
 };
 
 const server = new ApolloServer({
